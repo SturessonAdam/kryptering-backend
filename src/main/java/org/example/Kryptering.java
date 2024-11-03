ea93a3a82488363da616e305fa7c75bfed16c3d0
Adam <Adam.sturesson@live.se>
2024-11-03 20:35:35 +0100

    Kryptering class

---
+++ src/main/java/org/example/Kryptering.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Kryptering {
+
+    public String encrypt(String text) {
+        return null;
+    }
+
+    public String decrypt(String text) {
+        return null;
+    }
+}
